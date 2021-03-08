731ecf00b031b777e0a1058c841d164eb9df32d7
Andrew Mastbaum <mastbaum@physics.rutgers.edu>
2021-03-08 15:24:07 -0600

    improve sbn event weight
    - Improve data product class documentation
    - Add equaliy operators for parameters and parameter sets
    - Add dictionary types for art Assns

--- sbnobj/Common/SBNEventWeight/classes.h
+++ sbnobj/Common/SBNEventWeight/classes.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include "canvas/Persistency/Common/Wrapper.h"
+#include "canvas/Persistency/Common/Assns.h"
 #include "sbnobj/Common/SBNEventWeight/EventWeightMap.h"
 #include "sbnobj/Common/SBNEventWeight/EventWeightParameterSet.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
 
