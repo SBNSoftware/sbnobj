5dfeeed0cd873e27ecaa8944d27689d51cc51570
gputnam <gputnam@icarusgpvm03.fnal.gov>
2021-07-10 13:08:16 -0500

    Update documentation

--- sbnobj/Common/Reco/VertexHit.h
+++ sbnobj/Common/Reco/VertexHit.h
@@ -8,11 +8,11 @@
 namespace sbn {
   class VertexHit {
   public:
-    geo::WireID wire; //!< Wire the the hit is on
+    geo::WireID wire; //!< Wire that the hit is on
     float charge; //!< Calibrated and lifetime-corrected charge on the hit [#elec]
     float proj_dist_to_vertex; //!< Distnace from the hit to the vertex on the wireplane
-    float vtxw; //!< Wire of the vertex associated with this hit.
-    float vtxx; //!< X-Position of the vertex associated with this hit as seen by wire-planes.
+    float vtxw; //!< Wire of the vertex associated with this hit. Not space charge corrected. [cm]
+    float vtxx; //!< X-Position of the vertex associated with this hit as seen by wire-planes. Not space charge corrected. [cm]
     int spID; //!< ID of the SpacePoint associated with this hit
     geo::Point_t spXYZ; //!< 3D location of the SpacePoint associated with this hit. Space charge corrected. [cm]
     geo::Point_t vtxXYZ; //!< 3D location of the Vertex associated with this hit. Space charge corrected. [cm]
