3cbe4f142c4ab60afe45d627ee40b1fcb231e498
Gianluca Petrillo <petrillo.at.work@gmail.com>
2021-08-27 10:22:41 -0700

    Explicit initialization of CRTData members

--- sbnobj/ICARUS/CRT/CRTData.hh
+++ sbnobj/ICARUS/CRT/CRTData.hh
@@ -7,11 +7,11 @@ namespace icarus::crt {
 
   struct CRTData {
 
-      uint8_t  fMac5;    //FEB address following numbering convention common for both data and MC
-      uint32_t fEntry;   //hit index for given FEB in an event (starts from 0 for each event)
-      uint64_t fTs0;     //Absolute hit timestamp [ns]
-      uint64_t fTs1;     //Trigger time, not well defined as of Apr 14, 2021
-      uint16_t fAdc[64]; //ADC readout for each channel. CAEN (Bern) CRT FEBs use only indices 0-31
+      uint8_t  fMac5    { 0 }; ///< FEB address following numbering convention common for both data and MC.
+      uint32_t fEntry;         ///< Hit index for given FEB in an event (starts from 0 for each event).
+      uint64_t fTs0     { 0 }; ///< Absolute hit timestamp [ns]
+      uint64_t fTs1     { 0 }; ///< Trigger time, not well defined as of Apr 14, 2021.
+      uint16_t fAdc[64] {};    ///< ADC readout for each channel. CAEN (Bern) CRT FEBs use only indices 0-31.
       
   };
 
