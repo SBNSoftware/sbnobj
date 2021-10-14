7efec208271ae5dba13502a3812c843d83bcd2b6
gputnam <gputnam@icarusgpvm03.fnal.gov>
2021-10-14 16:58:05 -0500

    Save truth information per wire.

--- sbnobj/Common/Calibration/TrackCaloSkimmerObj.h
+++ sbnobj/Common/Calibration/TrackCaloSkimmerObj.h
@@ -23,6 +23,7 @@ namespace sbn {
     uint16_t wire; //!< Wire number
     uint16_t plane; //!< Plane number
     uint16_t tpc; //!< TPC number
+    uint16_t channel; //!< Channel number
     int16_t tdc0; //!< TDC tick of the first ADC value
     std::vector<short> adcs; //!< List of ADC values
 
@@ -41,6 +42,7 @@ namespace sbn {
     Vector3D p; //!< Position of hit [cm]
     float time; //!< Peak time of hit [ticks]
     int id; //!< ID of hit
+    uint16_t channel; //!< Channel number of hit
     uint16_t wire; //!< Wire number of hit
     uint16_t plane; //!< Plane number of hit
     uint16_t tpc; //!< TPC number of hit
@@ -54,6 +56,7 @@ namespace sbn {
       width(-1),
       time(-1),
       id(-1),
+      channel((uint16_t)-1),
       wire((uint16_t)-1),
       plane((uint16_t)-1),
       tpc((uint16_t)-1),
@@ -103,17 +106,22 @@ namespace sbn {
     int16_t tpc; //!< TPC of hit
     int16_t plane; //!< Plane of hit
     int wire; //!< Wire of hit
+    int channel; //!< Channel of hit
 
     unsigned ndep; //!< Number of depsotions in hit
     float nelec; //!< Number of electrons in hit
     float e; //!< energy in hit
     float pitch; //!< Track pitch for hit
+    float rr; //!< Track residual range for hit
+    int itraj; //!< Index of hit along trajectory
     Vector3D p; //!< Location of hit
+    float time; //!< Time of hit [ticks]
 
     TrueHit():
       cryo(-1),
       tpc(-1),
       wire(-1),
+      channel(-1),
       ndep(0),
       nelec(0.),
       e(0.),
@@ -164,7 +172,9 @@ namespace sbn {
     int   start_process; //!< Start G4 process of the particle. Values defned as enum in StandardRecord
     int   end_process; //!< End G4 process of the particle. Values defined as enum in StandardRecord
 
-    std::vector<TrueHit> truehits; //!< List of True "hits" of this particle
+    std::vector<TrueHit> truehits0; //!< List of True "hits" of this particle on Plane 0
+    std::vector<TrueHit> truehits1; //!< List of True "hits" of this particle on Plane 1
+    std::vector<TrueHit> truehits2; //!< List of True "hits" of this particle on Plane 2
 
     TrueParticle():
       plane0VisE(std::numeric_limits<float>::signaling_NaN()),
