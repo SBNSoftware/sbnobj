64120dab7f933a26468cefccae9bf58447e6a434
eyandel <eyandel16@gmail.com>
2021-11-11 09:26:51 -0600

    Created PMT Hardware Trigger simulation producer

---
+++ sbnobj/SBND/Trigger/classes.h
@@ -0,0 +1,6 @@
+#include "canvas/Persistency/Common/Wrapper.h"
+#include "canvas/Persistency/Common/Assns.h"
+#include "sbnobj/SBND/Trigger/pmtTrigger.hh"
+#include <vector>
+#include <map>
+#include <utility>
