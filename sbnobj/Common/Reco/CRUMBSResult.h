addce4367ab1012a566bac3021d7865d1b21890e
Henry Lay <h.lay@lancaster.ac.uk>
2022-02-22 04:31:02 -0600

    Create CRUMBSResult object

---
+++ sbnobj/Common/Reco/CRUMBSResult.h
@@ -0,0 +1,13 @@
+// Class for holding the result of the CRUMBS slice ID
+
+#ifndef sbncode_CRUMBSResult_H
+#define sbncode_CRUMBSResult_H
+
+namespace sbn {
+  class CRUMBSResult {
+  public:
+    float score;
+  };
+}
+
+#endif
