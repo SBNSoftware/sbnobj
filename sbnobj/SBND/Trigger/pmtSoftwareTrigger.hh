13da84dc01e1c55b481aa1cb6d3130b1c507d50a
lynnt-uchicago <lynnt@uchicago.edu>
2022-03-01 13:58:58 -0600

    draft pulse/pmt info

--- sbnobj/SBND/Trigger/pmtSoftwareTrigger.hh
+++ sbnobj/SBND/Trigger/pmtSoftwareTrigger.hh
@@ -9,11 +9,26 @@ namespace sbnd::trigger {
 
     size_t nAboveThreshold;
     int triggerTimestamp; // relative to beam window start, in ns
-
+    double totalPE;
     pmtSoftwareTrigger() {}
 
   };
 
+  struct pmtInfo{ 
+    double baseline;
+    double baselineSigma; 
+    int PE; 
+    pmtInfo() {}
+  };
+
+  struct pmtPulse{
+    double peak;
+    double area;
+    double t_start;
+    double t_peak;
+    double t_end;
+    pmtPulse() {}
+  };
 } // namespace sbnd::trigger
 
 #endif
