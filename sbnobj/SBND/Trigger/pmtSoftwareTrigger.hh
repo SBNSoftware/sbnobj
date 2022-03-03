d7b2d35095e3befd379d2abce94f44798a908cdd
lynnt-uchicago <lynnt@uchicago.edu>
2022-03-03 17:03:37 -0600

    added PE metrics

--- sbnobj/SBND/Trigger/pmtSoftwareTrigger.hh
+++ sbnobj/SBND/Trigger/pmtSoftwareTrigger.hh
@@ -2,25 +2,10 @@
 #define pmtSoftwareTrigger_h
 
 #include <cstddef>
+#include <vector>
+
 
 namespace sbnd::trigger {
-
-  struct pmtSoftwareTrigger{
-
-    bool foundBeamTrigger;
-    int triggerTimestamp; // relative to beam window start, in ns
-    double totalPE;
-    size_t nAboveThreshold;
-    pmtSoftwareTrigger() {}
-
-  };
-
-  struct pmtInfo{ 
-    double baseline;
-    double baselineSigma; 
-    int PE; 
-    pmtInfo() {}
-  };
 
   struct pmtPulse{
     double peak;
@@ -28,8 +13,29 @@ namespace sbnd::trigger {
     double t_start;
     double t_peak;
     double t_end;
+    double pe;
     pmtPulse() {}
   };
+
+  struct pmtInfo{ 
+    int channel;
+    double baseline;
+    double baselineSigma; 
+    std::vector<sbnd::trigger::pmtPulse> pulseVec;
+    pmtInfo() {}
+  };
+
+  struct pmtSoftwareTrigger{
+
+    bool foundBeamTrigger;
+    int triggerTimestamp; // relative to beam window start, in ns
+    double promptPE; // total PE for the 100 ns following trigger time 
+    double prelimPE; // total PE for the 1 us before trigger time
+    int nAboveThreshold;
+    std::vector<sbnd::trigger::pmtInfo> pmtInfoVec;
+    pmtSoftwareTrigger() {}
+  };
+
 } // namespace sbnd::trigger
 
 #endif
