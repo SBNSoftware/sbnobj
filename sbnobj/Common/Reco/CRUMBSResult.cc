aa993b1ae9aaa9759736fa8eda7c730213e1cd8a
Henry Lay <h.lay@lancaster.ac.uk>
2022-03-17 09:59:56 -0500

    Sort constructors, default values and documentation

--- sbnobj/Common/Reco/CRUMBSResult.cc
+++ sbnobj/Common/Reco/CRUMBSResult.cc
@@ -1,13 +1,11 @@
 #include "sbnobj/Common/Reco/CRUMBSResult.h"
 
-#include <limits>
-
 sbn::CRUMBSResult::CRUMBSResult(float score, float tpc_CRFracHitsInLongestTrack, float tpc_CRLongestTrackDeflection, float tpc_CRLongestTrackDirY,
 				int tpc_CRNHitsMax, float tpc_NuEigenRatioInSphere, int tpc_NuNFinalStatePfos, int tpc_NuNHitsTotal,
 				int tpc_NuNSpacePointsInSphere, float tpc_NuVertexY, float tpc_NuWeightedDirZ, float tpc_StoppingChi2CosmicRatio,
 				float pds_FMTotalScore, float pds_FMPE, float pds_FMTime, float crt_TrackScore, float crt_HitScore,
 				float crt_TrackTime, float crt_HitTime)
-: score(score)
+  : score(score)
   , tpc_CRFracHitsInLongestTrack(tpc_CRFracHitsInLongestTrack)
   , tpc_CRLongestTrackDeflection(tpc_CRLongestTrackDeflection)
   , tpc_CRLongestTrackDirY(tpc_CRLongestTrackDirY)
@@ -29,30 +27,3 @@ sbn::CRUMBSResult::CRUMBSResult(float score, float tpc_CRFracHitsInLongestTrack,
 {
 }
 
-sbn::CRUMBSResult::CRUMBSResult(float score)
-  : score(score)
-{
-}
-
-sbn::CRUMBSResult::CRUMBSResult()
-  : score(std::numeric_limits<float>::signaling_NaN())
-  , tpc_CRFracHitsInLongestTrack(std::numeric_limits<float>::signaling_NaN())
-  , tpc_CRLongestTrackDeflection(std::numeric_limits<float>::signaling_NaN())
-  , tpc_CRLongestTrackDirY(std::numeric_limits<float>::signaling_NaN())
-  , tpc_CRNHitsMax(std::numeric_limits<int>::max())
-  , tpc_NuEigenRatioInSphere(std::numeric_limits<float>::signaling_NaN())
-  , tpc_NuNFinalStatePfos(std::numeric_limits<int>::max())
-  , tpc_NuNHitsTotal(std::numeric_limits<int>::max())
-  , tpc_NuNSpacePointsInSphere(std::numeric_limits<int>::max())
-  , tpc_NuVertexY(std::numeric_limits<float>::signaling_NaN())
-  , tpc_NuWeightedDirZ(std::numeric_limits<float>::signaling_NaN())
-  , tpc_StoppingChi2CosmicRatio(std::numeric_limits<float>::signaling_NaN())
-  , pds_FMTotalScore(std::numeric_limits<float>::signaling_NaN())
-  , pds_FMPE(std::numeric_limits<float>::signaling_NaN())
-  , pds_FMTime(std::numeric_limits<float>::signaling_NaN())
-  , crt_TrackScore(std::numeric_limits<float>::signaling_NaN())
-  , crt_HitScore(std::numeric_limits<float>::signaling_NaN())
-  , crt_TrackTime(std::numeric_limits<float>::signaling_NaN())
-  , crt_HitTime(std::numeric_limits<float>::signaling_NaN())
-{
-}
