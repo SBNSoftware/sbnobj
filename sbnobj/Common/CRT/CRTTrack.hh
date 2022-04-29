bea675c1d4ef8c28f039c716677dd628d9325773
Marco Del Tutto <mdeltutt@fnal.gov>
2022-04-29 15:13:08 -0500

    Add comments to variables

--- sbnobj/Common/CRT/CRTTrack.hh
+++ sbnobj/Common/CRT/CRTTrack.hh
@@ -22,37 +22,37 @@ namespace sbn::crt {
 
     std::vector<uint8_t> feb_id;
     std::map< uint8_t, std::vector<std::pair<int,float> > > pesmap;
-    float peshit;
-    double ts0_s;
-    double ts0_s_err;
-    double ts0_ns;
-    double ts0_ns_err;
-    double ts1_ns;
-    double ts1_ns_err;
-    int plane1;
-    int plane2;
+    float peshit; ///< Total photoelectrons for this track (sum of PEs from the two CRTHits)
+    double ts0_s; ///< Average time (second) of the two hits making the track
+    double ts0_s_err; ///< Average time (second) spread of the two hits making the track
+    double ts0_ns; ///< Average T0 (nanosecond) of the two hits making the track
+    double ts0_ns_err; ///< Error on average T0 (nanosecond) of the two hits making the track
+    double ts1_ns; ///< Average T1 (nanosecond) of the two hits making the track
+    double ts1_ns_err; ///< Error on average T1 (nanosecond) of the two hits making the track
+    int plane1; ///< Plane ID of first CRTHit
+    int plane2; ///< Plane ID of second CRTHit
 
-    float x1_pos;
-    float x1_err;
-    float y1_pos;
-    float y1_err;
-    float z1_pos;
-    float z1_err;
-    float x2_pos;
-    float x2_err;
-    float y2_pos;
-    float y2_err;
-    float z2_pos;
-    float z2_err;
-    float length;
-    float thetaxy;
-    float phizy;
-    double ts0_ns_h1;
-    double ts0_ns_err_h1;
-    double ts0_ns_h2;
-    double ts0_ns_err_h2;
+    float x1_pos; ///< X position of first CRTHit
+    float x1_err; ///< X position error of first CRTHit
+    float y1_pos; ///< Y position of first CRTHit
+    float y1_err; ///< Y position error of first CRTHit
+    float z1_pos; ///< Z position of first CRTHit
+    float z1_err; ///< Z position error of first CRTHit
+    float x2_pos; ///< X position of second CRTHit
+    float x2_err; ///< X position error of second CRTHit
+    float y2_pos; ///< Y position of second CRTHit
+    float y2_err; ///< Y position error of second CRTHit
+    float z2_pos; ///< Z position of second CRTHit
+    float z2_err; ///< Z position error of second CRTHit
+    float length; ///< Track length
+    float thetaxy;  ///< Track angle on the X-Y plane
+    float phizy; ///< Track angle on the Z-Y plane
+    double ts0_ns_h1; ///< T0 time of first CRTHit
+    double ts0_ns_err_h1; ///< T0 time error of first CRTHit
+    double ts0_ns_h2; ///< T0 time of second CRTHit
+    double ts0_ns_err_h2; ///< T0 time error of second CRTHit
 
-    bool complete;
+    bool complete; ///< Whether or not the track is complete
 
     CRTTrack() {}
 
