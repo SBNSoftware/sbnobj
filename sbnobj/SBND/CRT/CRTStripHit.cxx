0cddaa296dbbd9286acdffb265ac13c712af6922
Henry Lay <h.lay@lancaster.ac.uk>
2022-11-06 21:39:11 -0600

    Add global position to strip hit

--- sbnobj/SBND/CRT/CRTStripHit.cxx
+++ sbnobj/SBND/CRT/CRTStripHit.cxx
@@ -12,22 +12,26 @@ namespace sbnd {
       , ts0          (0)
       , ts1          (0)
       , s            (0)
-      , x            (0)
-      , ex           (0)
+      , pos          (0)
+      , err          (0)
       , adc1         (0)
       , adc2         (0)
+      , xyz          (0, 0, 0)
+      , exyz         (0, 0, 0)
     {}
 
-    CRTStripHit::CRTStripHit(uint32_t _channel, uint32_t _ts0, uint32_t _ts1, uint32_t _s, double _x,
-			     double _ex, uint16_t _adc1, uint16_t _adc2)
+    CRTStripHit::CRTStripHit(uint32_t _channel, uint32_t _ts0, uint32_t _ts1, uint32_t _s, double _pos,
+			     double _err, uint16_t _adc1, uint16_t _adc2, TVector3 _xyz, TVector3 _exyz)
       : channel      (_channel)
       , ts0          (_ts0)
       , ts1          (_ts1)
       , s            (_s)
-      , x            (_x)
-      , ex           (_ex)
+      , pos          (_pos)
+      , err          (_err)
       , adc1         (_adc1)
       , adc2         (_adc2)
+      , xyz          (_xyz)
+      , exyz         (_exyz)
     {}
 
     CRTStripHit::~CRTStripHit() {}
@@ -36,10 +40,12 @@ namespace sbnd {
     uint32_t CRTStripHit::Ts0() { return ts0; }
     uint32_t CRTStripHit::Ts1() { return ts1; }
     uint32_t CRTStripHit::UnixS() { return s; }
-    double   CRTStripHit::X() { return x; }
-    double   CRTStripHit::X_Error() { return ex; }
+    double   CRTStripHit::Pos() { return pos; }
+    double   CRTStripHit::Error() { return err; }
     uint16_t CRTStripHit::ADC1() { return adc1; }
     uint16_t CRTStripHit::ADC2() { return adc2; }
+    TVector3 CRTStripHit::XYZ() { return xyz; }
+    TVector3 CRTStripHit::XYZ_Error() { return exyz; }
   }
 }
 
--- sbnobj/SBND/CRT/CRTStripHit.hh
+++ sbnobj/SBND/CRT/CRTStripHit.hh
@@ -11,6 +11,7 @@
 #define SBND_CRTSTRIPHIT_HH
 
 #include <stdint.h>
+#include "TVector3.h"
 
 namespace sbnd::crt {
 
@@ -20,17 +21,19 @@ namespace sbnd::crt {
     uint32_t ts0;          // T0 counter [ns]
     uint32_t ts1;          // T1 counter [ns]
     uint32_t s;            // Unixtime of event [s]
-    double   x;            // Lateral position within strip [cm]
-    double   ex;           // Error on lateral position [cm]
+    double   pos;          // Lateral position within strip [cm]
+    double   err;          // Error on lateral position [cm]
     uint16_t adc1;         // ADC 1st SiPM
     uint16_t adc2;         // ADC 2nd SiPM
+    TVector3 xyz;          // Global position [cm]
+    TVector3 exyz;         // Global position error [cm]
 
   public:
 
     CRTStripHit();
     
-    CRTStripHit(uint32_t _channel, uint32_t _ts0, uint32_t _ts1, uint32_t _s, double _x,
-		double _ex, uint16_t _adc1, uint16_t _adc2);
+    CRTStripHit(uint32_t _channel, uint32_t _ts0, uint32_t _ts1, uint32_t _s, double _pos,
+		double _err, uint16_t _adc1, uint16_t _adc2, TVector3 _xyz, TVector3 _exyz);
 
     virtual ~CRTStripHit();
 
@@ -38,10 +41,12 @@ namespace sbnd::crt {
     uint32_t Ts0();
     uint32_t Ts1();
     uint32_t UnixS();
-    double   X();
-    double   X_Error();
+    double   Pos();
+    double   Error();
     uint16_t ADC1();
     uint16_t ADC2();
+    TVector3 XYZ();
+    TVector3 XYZ_Error();
   };
 }
 
