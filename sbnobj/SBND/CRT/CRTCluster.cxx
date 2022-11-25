00f354b543c5903791bec956f7747e6d5659d984
Henry Lay <h.lay@lancaster.ac.uk>
2022-11-24 18:54:09 -0600

    Simplify cluster

--- sbnobj/SBND/CRT/CRTCluster.cxx
+++ sbnobj/SBND/CRT/CRTCluster.cxx
@@ -11,51 +11,18 @@ namespace sbnd {
       : ts0          (0)
       , ts1          (0)
       , s            (0)
-      , minX         (0.)
-      , maxX         (0.)
-      , minY         (0.)
-      , maxY         (0.)
-      , minZ         (0.)
-      , maxZ         (0.)
       , nHits        (0)
-      , sumADC       (0)
-      , sumADCCorr   (0)
       , tagger       ("")
+      , threeD       (false)
     {}
 
-    CRTCluster::CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, double _minX, double _maxX,
-			   double _minY, double _maxY, double _minZ, double _maxZ, uint16_t _nHits,
-			   uint16_t _sumADC, uint16_t _sumADCCorr, std::string _tagger)
+    CRTCluster::CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, uint16_t _nHits, std::string _tagger, bool _threeD)
       : ts0          (_ts0)
       , ts1          (_ts1)
       , s            (_s)
-      , minX         (_minX)
-      , maxX         (_maxX)
-      , minY         (_minY)
-      , maxY         (_maxY)
-      , minZ         (_minZ)
-      , maxZ         (_maxZ)
       , nHits        (_nHits)
-      , sumADC       (_sumADC)
-      , sumADCCorr   (_sumADCCorr)
       , tagger       (_tagger)
-    {}
-
-    CRTCluster::CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, std::array<double, 6> _edges,
-			   uint16_t _nHits, uint16_t _sumADC, uint16_t _sumADCCorr, std::string _tagger)
-      : ts0          (_ts0)
-      , ts1          (_ts1)
-      , s            (_s)
-      , minX         (_edges[0])
-      , maxX         (_edges[1])
-      , minY         (_edges[2])
-      , maxY         (_edges[3])
-      , minZ         (_edges[4])
-      , maxZ         (_edges[5])
-      , nHits        (_nHits)
-      , sumADC       (_sumADC)
-      , sumADCCorr   (_sumADCCorr)
-      , tagger       (_tagger)
+      , threeD       (_threeD)
     {}
 
     CRTCluster::~CRTCluster() {}
@@ -63,16 +30,9 @@ namespace sbnd {
     uint32_t    CRTCluster::Ts0() const { return ts0; }
     uint32_t    CRTCluster::Ts1() const { return ts1; }
     uint32_t    CRTCluster::UnixS() const { return s; }
-    double      CRTCluster::MinX() const { return minX; }
-    double      CRTCluster::MaxX() const { return maxX; }
-    double      CRTCluster::MinY() const { return minY; }
-    double      CRTCluster::MaxY() const { return maxY; }
-    double      CRTCluster::MinZ() const { return minZ; }
-    double      CRTCluster::MaxZ() const { return maxZ; }
     uint16_t    CRTCluster::NHits() const { return nHits; }
-    uint16_t    CRTCluster::SumADC() const { return sumADC; }
-    uint16_t    CRTCluster::SumADCCorr() const { return sumADCCorr; }
     std::string CRTCluster::Tagger() const { return tagger; }
+    bool        CRTCluster::ThreeD() const { return threeD; }
   }
 }
 
--- sbnobj/SBND/CRT/CRTCluster.hh
+++ sbnobj/SBND/CRT/CRTCluster.hh
@@ -21,44 +21,24 @@ namespace sbnd::crt {
     uint32_t    ts0;          // T0 counter [ns]
     uint32_t    ts1;          // T1 counter [ns]
     uint32_t    s;            // Unixtime of event [s]
-    double      minX;         // Minimum X position within cluster [cm]
-    double      maxX;         // Maximum X position within cluster [cm]
-    double      minY;         // Minimum Y position within cluster [cm]
-    double      maxY;         // Maximum Y position within cluster [cm]
-    double      minZ;         // Minimum Z position within cluster [cm]
-    double      maxZ;         // Maximum Z position within cluster [cm]
     uint16_t    nHits;        // The number of strip hits forming the cluster
-    uint16_t    sumADC;       // The sum of the ADC counts from the contributing hits
-    uint16_t    sumADCCorr;   // The sum of the ADC counts from the contributing hits,
-                              //   with corrections applied
     std::string tagger;       // The name of the tagger this cluster exists on
+    bool        threeD;       // Does the cluster contain strips from both orientations?
 
   public:
 
     CRTCluster();
     
-    CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, double _minX, double _maxX,
-	       double _minY, double _maxY, double _minZ, double _maxZ, uint16_t _nHits,
-	       uint16_t _sumADC, uint16_t _sumADCCorr, std::string _tagger);
-
-    CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, std::array<double, 6> _edges,
-               uint16_t _nHits, uint16_t _sumADC, uint16_t _sumADCCorr, std::string _tagger);
+    CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, uint16_t _nHits, std::string _tagger, bool _threeD);
 
     virtual ~CRTCluster();
 
     uint32_t    Ts0() const;
     uint32_t    Ts1() const;
     uint32_t    UnixS() const;
-    double      MinX() const;
-    double      MaxX() const;
-    double      MinY() const;
-    double      MaxY() const;
-    double      MinZ() const;
-    double      MaxZ() const;
     uint16_t    NHits() const;
-    uint16_t    SumADC() const;
-    uint16_t    SumADCCorr() const;
     std::string Tagger() const;
+    bool        ThreeD() const;
   };
 }
 
