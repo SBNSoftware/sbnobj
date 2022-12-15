a4e0430fa1a61be03b2cc9407c931d9e72fea5d7
Henry Lay <h.lay@lancaster.ac.uk>
2022-12-15 09:50:35 -0600

    Add enum for tagger

--- sbnobj/SBND/CRT/CRTCluster.cxx
+++ sbnobj/SBND/CRT/CRTCluster.cxx
@@ -12,11 +12,11 @@ namespace sbnd {
       , ts1          (0)
       , s            (0)
       , nHits        (0)
-      , tagger       ("")
+      , tagger       (kUndefinedTagger)
       , threeD       (false)
     {}
 
-    CRTCluster::CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, uint16_t _nHits, std::string _tagger, bool _threeD)
+    CRTCluster::CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, uint16_t _nHits, CRTTagger _tagger, bool _threeD)
       : ts0          (_ts0)
       , ts1          (_ts1)
       , s            (_s)
@@ -27,12 +27,12 @@ namespace sbnd {
 
     CRTCluster::~CRTCluster() {}
 
-    uint32_t    CRTCluster::Ts0() const { return ts0; }
-    uint32_t    CRTCluster::Ts1() const { return ts1; }
-    uint32_t    CRTCluster::UnixS() const { return s; }
-    uint16_t    CRTCluster::NHits() const { return nHits; }
-    std::string CRTCluster::Tagger() const { return tagger; }
-    bool        CRTCluster::ThreeD() const { return threeD; }
+    uint32_t  CRTCluster::Ts0() const { return ts0; }
+    uint32_t  CRTCluster::Ts1() const { return ts1; }
+    uint32_t  CRTCluster::UnixS() const { return s; }
+    uint16_t  CRTCluster::NHits() const { return nHits; }
+    CRTTagger CRTCluster::Tagger() const { return tagger; }
+    bool      CRTCluster::ThreeD() const { return threeD; }
   }
 }
 
--- sbnobj/SBND/CRT/CRTCluster.hh
+++ sbnobj/SBND/CRT/CRTCluster.hh
@@ -16,29 +16,43 @@
 
 namespace sbnd::crt {
 
+  enum CRTTagger {
+    kUndefinedTagger = -1,
+    kBottomTagger,
+    kSouthTagger,
+    kNorthTagger,
+    kWestTagger,
+    kEastTagger,
+    kTopLowTagger,
+    kTopHighTagger,
+
+    kUpstreamTagger   = kSouthTagger,
+    kDownstreamTagger = kNorthTagger
+  };
+
   class CRTCluster{
     
-    uint32_t    ts0;          // T0 counter [ns]
-    uint32_t    ts1;          // T1 counter [ns]
-    uint32_t    s;            // Unixtime of event [s]
-    uint16_t    nHits;        // The number of strip hits forming the cluster
-    std::string tagger;       // The name of the tagger this cluster exists on
-    bool        threeD;       // Does the cluster contain strips from both orientations?
+    uint32_t  ts0;          // T0 counter [ns]
+    uint32_t  ts1;          // T1 counter [ns]
+    uint32_t  s;            // Unixtime of event [s]
+    uint16_t  nHits;        // The number of strip hits forming the cluster
+    CRTTagger tagger;       // The tagger this cluster exists on
+    bool      threeD;       // Does the cluster contain strips from both orientations?
 
   public:
 
     CRTCluster();
     
-    CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, uint16_t _nHits, std::string _tagger, bool _threeD);
+    CRTCluster(uint32_t _ts0, uint32_t _ts1, uint32_t _s, uint16_t _nHits, CRTTagger _tagger, bool _threeD);
 
     virtual ~CRTCluster();
 
-    uint32_t    Ts0() const;
-    uint32_t    Ts1() const;
-    uint32_t    UnixS() const;
-    uint16_t    NHits() const;
-    std::string Tagger() const;
-    bool        ThreeD() const;
+    uint32_t  Ts0() const;
+    uint32_t  Ts1() const;
+    uint32_t  UnixS() const;
+    uint16_t  NHits() const;
+    CRTTagger Tagger() const;
+    bool      ThreeD() const;
   };
 }
 
