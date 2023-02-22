6ebda896f64af181990a8ab672ed889aa6c3c542
Henry Lay <h.lay@lancaster.ac.uk>
2023-02-22 13:28:49 -0600

    Add length and tof to track object

--- sbnobj/SBND/CRT/CRTTrack.cxx
+++ sbnobj/SBND/CRT/CRTTrack.cxx
@@ -13,6 +13,8 @@ namespace sbnd {
       , time    (0.)
       , etime   (0.)
       , pe      (0.)
+      , length  (0.)
+      , tof     (0.)
       , triple  (false)
       , taggers ()
     {
@@ -20,23 +22,29 @@ namespace sbnd {
     }
 
     CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-                       const double &_pe, const bool &_triple, const std::array<bool, 7> &_taggers)
+                       const double &_pe, const double &_length, const double &_tof, const bool &_triple, 
+                       const std::array<bool, 7> &_taggers)
       : start   (_start)
       , dir     (_dir)
       , time    (_time)
       , etime   (_etime)
       , pe      (_pe)
+      , length  (_length)
+      , tof     (_tof)
       , triple  (_triple)
       , taggers (_taggers)
     {}
 
     CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-                       const double &_pe, const bool &_triple, const std::vector<CRTTagger> &_used_taggers)
+                       const double &_pe, const double &_length, const double &_tof, const bool &_triple, 
+                       const std::set<CRTTagger> &_used_taggers)
       : start   (_start)
       , dir     (_dir)
       , time    (_time)
       , etime   (_etime)
       , pe      (_pe)
+      , length  (_length)
+      , tof     (_tof)
       , triple  (_triple)
       , taggers ()
     {
@@ -51,8 +59,13 @@ namespace sbnd {
     double              CRTTrack::Time() const {return time; }
     double              CRTTrack::TimeErr() const { return etime; }
     double              CRTTrack::PE() const { return pe; }
+    double              CRTTrack::Length() const { return length; }
+    double              CRTTrack::ToF() const { return tof; }
     bool                CRTTrack::Triple() const { return triple; }
     std::array<bool, 7> CRTTrack::Taggers() const { return taggers; }
+
+    double CRTTrack::Theta() const { return dir.Theta(); }
+    double CRTTrack::Phi() const { return dir.Phi(); }
 
     bool CRTTrack::UsedTagger(const CRTTagger tagger) const { return taggers[tagger]; }
   }
--- sbnobj/SBND/CRT/CRTTrack.hh
+++ sbnobj/SBND/CRT/CRTTrack.hh
@@ -13,15 +13,19 @@
 #include "larcoreobj/SimpleTypesAndConstants/geo_vectors.h"
 #include "sbnobj/SBND/CRT/CRTEnums.hh"
 
+#include <set>
+
 namespace sbnd::crt {
 
   class CRTTrack {
     
     geo::Point_t        start;   // the fitted start position [cm]
     geo::Vector_t       dir;     // the fitted track direction [cm]
-    double              time;    // time [ns]
-    double              etime;   // time error [ns]
+    double              time;    // average time [ns]
+    double              etime;   // average time error [ns]
     double              pe;      // total PE
+    double              length;  // track length [cm]
+    double              tof;     // time from first space point to last [ns]
     bool                triple;  // whether the track contains space points from 3 taggers
     std::array<bool, 7> taggers; // whether the track contains a space point from each tagger
                                  // (enumerated by CRTTagger enum)
@@ -31,10 +35,12 @@ namespace sbnd::crt {
     CRTTrack();
     
     CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-             const double &_pe, const bool &_triple, const std::array<bool, 7> &_taggers);
+             const double &_pe, const double &_length, const double &_tof, const bool &_triple,
+             const std::array<bool, 7> &_taggers);
 
     CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-             const double &_pe, const bool &_triple, const std::vector<CRTTagger> &_used_taggers);
+             const double &_pe, const double &_length, const double &_tof, const bool &_triple,
+             const std::set<CRTTagger> &_used_taggers);
 
     virtual ~CRTTrack();
 
@@ -43,8 +49,13 @@ namespace sbnd::crt {
     double              Time() const;
     double              TimeErr() const;
     double              PE() const;
+    double              Length() const;
+    double              ToF() const;
     bool                Triple() const;
     std::array<bool, 7> Taggers() const;
+
+    double Theta() const;
+    double Phi() const;
     
     bool UsedTagger(const CRTTagger tagger) const;
   };
