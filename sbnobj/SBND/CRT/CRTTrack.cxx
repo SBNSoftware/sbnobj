b3b1e11f5f97e38b315b1c26c96c63687b22f2aa
Henry Lay <h.lay@lancaster.ac.uk>
2023-02-23 14:15:26 -0600

    Swap track philosophy to store start & end not start & direction

--- sbnobj/SBND/CRT/CRTTrack.cxx
+++ sbnobj/SBND/CRT/CRTTrack.cxx
@@ -9,11 +9,10 @@ namespace sbnd {
 
     CRTTrack::CRTTrack()
       : start   ({0., 0., 0.})
-      , dir     ({0., 0., 0.})
+      , end     ({0., 0., 0.})
       , time    (0.)
       , etime   (0.)
       , pe      (0.)
-      , length  (0.)
       , tof     (0.)
       , triple  (false)
       , taggers ()
@@ -21,29 +20,27 @@ namespace sbnd {
       taggers.fill(false);
     }
 
-    CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-                       const double &_pe, const double &_length, const double &_tof, const bool &_triple, 
+    CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Point_t &_end, const double &_time, const double &_etime,
+                       const double &_pe, const double &_tof, const bool &_triple,
                        const std::array<bool, 7> &_taggers)
       : start   (_start)
-      , dir     (_dir)
+      , end     (_end)
       , time    (_time)
       , etime   (_etime)
       , pe      (_pe)
-      , length  (_length)
       , tof     (_tof)
       , triple  (_triple)
       , taggers (_taggers)
     {}
 
-    CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-                       const double &_pe, const double &_length, const double &_tof, const bool &_triple, 
+    CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Point_t &_end, const double &_time, const double &_etime,
+                       const double &_pe, const double &_tof, const bool &_triple,
                        const std::set<CRTTagger> &_used_taggers)
       : start   (_start)
-      , dir     (_dir)
+      , end     (_end)
       , time    (_time)
       , etime   (_etime)
       , pe      (_pe)
-      , length  (_length)
       , tof     (_tof)
       , triple  (_triple)
       , taggers ()
@@ -55,17 +52,18 @@ namespace sbnd {
     CRTTrack::~CRTTrack() {}
 
     geo::Point_t        CRTTrack::Start() const { return start; }
-    geo::Vector_t       CRTTrack::Direction() const { return dir; }
+    geo::Point_t        CRTTrack::End() const { return end; }
     double              CRTTrack::Time() const {return time; }
     double              CRTTrack::TimeErr() const { return etime; }
     double              CRTTrack::PE() const { return pe; }
-    double              CRTTrack::Length() const { return length; }
     double              CRTTrack::ToF() const { return tof; }
     bool                CRTTrack::Triple() const { return triple; }
     std::array<bool, 7> CRTTrack::Taggers() const { return taggers; }
 
-    double CRTTrack::Theta() const { return dir.Theta(); }
-    double CRTTrack::Phi() const { return dir.Phi(); }
+    geo::Vector_t CRTTrack::Direction() const { return (end - start).Unit(); }
+    double        CRTTrack::Length() const { return (end - start).R(); }
+    double        CRTTrack::Theta() const { return (end - start).Theta(); }
+    double        CRTTrack::Phi() const { return (end - start).Phi(); }
 
     bool CRTTrack::UsedTagger(const CRTTagger tagger) const { return taggers[tagger]; }
   }
--- sbnobj/SBND/CRT/CRTTrack.hh
+++ sbnobj/SBND/CRT/CRTTrack.hh
@@ -20,11 +20,10 @@ namespace sbnd::crt {
   class CRTTrack {
     
     geo::Point_t        start;   // the fitted start position [cm]
-    geo::Vector_t       dir;     // the fitted track direction [cm]
+    geo::Point_t        end;     // the fitted track end [cm]
     double              time;    // average time [ns]
     double              etime;   // average time error [ns]
     double              pe;      // total PE
-    double              length;  // track length [cm]
     double              tof;     // time from first space point to last [ns]
     bool                triple;  // whether the track contains space points from 3 taggers
     std::array<bool, 7> taggers; // whether the track contains a space point from each tagger
@@ -34,28 +33,29 @@ namespace sbnd::crt {
 
     CRTTrack();
     
-    CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-             const double &_pe, const double &_length, const double &_tof, const bool &_triple,
+    CRTTrack(const geo::Point_t &_start, const geo::Point_t &_dir, const double &_time, const double &_etime,
+             const double &_pe, const double &_tof, const bool &_triple,
              const std::array<bool, 7> &_taggers);
 
-    CRTTrack(const geo::Point_t &_start, const geo::Vector_t &_dir, const double &_time, const double &_etime,
-             const double &_pe, const double &_length, const double &_tof, const bool &_triple,
+    CRTTrack(const geo::Point_t &_start, const geo::Point_t &_dir, const double &_time, const double &_etime,
+             const double &_pe, const double &_tof, const bool &_triple,
              const std::set<CRTTagger> &_used_taggers);
 
     virtual ~CRTTrack();
 
     geo::Point_t        Start() const;
-    geo::Vector_t       Direction() const;
+    geo::Point_t        End() const;
     double              Time() const;
     double              TimeErr() const;
     double              PE() const;
-    double              Length() const;
     double              ToF() const;
     bool                Triple() const;
     std::array<bool, 7> Taggers() const;
 
-    double Theta() const;
-    double Phi() const;
+    geo::Vector_t Direction() const;
+    double        Length() const;
+    double        Theta() const;
+    double        Phi() const;
     
     bool UsedTagger(const CRTTagger tagger) const;
   };
