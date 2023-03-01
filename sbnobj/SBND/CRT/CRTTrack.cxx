fce3f3f27febee09b21077d4ec81f098f641117d
Henry Lay <h.lay@lancaster.ac.uk>
2023-03-01 14:15:10 -0600

    Make recommended changes

--- sbnobj/SBND/CRT/CRTTrack.cxx
+++ sbnobj/SBND/CRT/CRTTrack.cxx
@@ -8,64 +8,52 @@ namespace sbnd {
   namespace crt {
 
     CRTTrack::CRTTrack()
-      : start   ({0., 0., 0.})
-      , end     ({0., 0., 0.})
+      : points  ({})
       , time    (0.)
       , etime   (0.)
       , pe      (0.)
       , tof     (0.)
-      , triple  (false)
-      , taggers ()
-    {
-      taggers.fill(false);
-    }
-
-    CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Point_t &_end, const double &_time, const double &_etime,
-                       const double &_pe, const double &_tof, const bool &_triple,
-                       const std::array<bool, 7> &_taggers)
-      : start   (_start)
-      , end     (_end)
-      , time    (_time)
-      , etime   (_etime)
-      , pe      (_pe)
-      , tof     (_tof)
-      , triple  (_triple)
-      , taggers (_taggers)
+      , taggers ({})
     {}
 
     CRTTrack::CRTTrack(const geo::Point_t &_start, const geo::Point_t &_end, const double &_time, const double &_etime,
-                       const double &_pe, const double &_tof, const bool &_triple,
-                       const std::set<CRTTagger> &_used_taggers)
-      : start   (_start)
-      , end     (_end)
+                       const double &_pe, const double &_tof, const std::set<CRTTagger> &_taggers)
+      : points  ({_start, _end})
       , time    (_time)
       , etime   (_etime)
       , pe      (_pe)
       , tof     (_tof)
-      , triple  (_triple)
-      , taggers ()
-    {
-      for(auto const &tagger : _used_taggers)
-        taggers[tagger] = true;
-    }
+      , taggers (_taggers)
+    {}
+
+    CRTTrack::CRTTrack(const std::vector<geo::Point_t> &_points, const double &_time, const double &_etime,
+                       const double &_pe, const double &_tof, const std::set<CRTTagger> &_taggers)
+      : points  (_points)
+      , time    (_time)
+      , etime   (_etime)
+      , pe      (_pe)
+      , tof     (_tof)
+      , taggers (_taggers)
+    {}
 
     CRTTrack::~CRTTrack() {}
 
-    geo::Point_t        CRTTrack::Start() const { return start; }
-    geo::Point_t        CRTTrack::End() const { return end; }
-    double              CRTTrack::Time() const {return time; }
-    double              CRTTrack::TimeErr() const { return etime; }
-    double              CRTTrack::PE() const { return pe; }
-    double              CRTTrack::ToF() const { return tof; }
-    bool                CRTTrack::Triple() const { return triple; }
-    std::array<bool, 7> CRTTrack::Taggers() const { return taggers; }
+    std::vector<geo::Point_t> CRTTrack::Points() const { return points; }
+    double                    CRTTrack::Time() const {return time; }
+    double                    CRTTrack::TimeErr() const { return etime; }
+    double                    CRTTrack::PE() const { return pe; }
+    double                    CRTTrack::ToF() const { return tof; }
+    std::set<CRTTagger>       CRTTrack::Taggers() const { return taggers; }
 
-    geo::Vector_t CRTTrack::Direction() const { return (end - start).Unit(); }
-    double        CRTTrack::Length() const { return (end - start).R(); }
-    double        CRTTrack::Theta() const { return (end - start).Theta(); }
-    double        CRTTrack::Phi() const { return (end - start).Phi(); }
+    geo::Point_t  CRTTrack::Start() const { return points.front(); }
+    geo::Point_t  CRTTrack::End() const { return points.back(); }
+    geo::Vector_t CRTTrack::Direction() const { return (End() - Start()).Unit(); }
+    double        CRTTrack::Length() const { return (End() - Start()).R(); }
+    double        CRTTrack::Theta() const { return (End() - Start()).Theta(); }
+    double        CRTTrack::Phi() const { return (End() - Start()).Phi(); }
+    bool          CRTTrack::Triple() const { return taggers.size() == 3; }
 
-    bool CRTTrack::UsedTagger(const CRTTagger tagger) const { return taggers[tagger]; }
+    bool CRTTrack::UsedTagger(const CRTTagger tagger) const { return taggers.count(tagger) == 1; }
   }
 }
 
--- sbnobj/SBND/CRT/CRTTrack.hh
+++ sbnobj/SBND/CRT/CRTTrack.hh
@@ -19,43 +19,39 @@ namespace sbnd::crt {
 
   class CRTTrack {
     
-    geo::Point_t        start;   // the fitted start position [cm]
-    geo::Point_t        end;     // the fitted track end [cm]
-    double              time;    // average time [ns]
-    double              etime;   // average time error [ns]
-    double              pe;      // total PE
-    double              tof;     // time from first space point to last [ns]
-    bool                triple;  // whether the track contains space points from 3 taggers
-    std::array<bool, 7> taggers; // whether the track contains a space point from each tagger
-                                 // (enumerated by CRTTagger enum)
+    std::vector<geo::Point_t> points;  // the fitted track points at each tagger [cm]
+    double                    time;    // average time [ns]
+    double                    etime;   // average time error [ns]
+    double                    pe;      // total PE
+    double                    tof;     // time from first space point to last [ns]
+    std::set<CRTTagger>       taggers; // which taggers were used to create the track
 
   public:
 
     CRTTrack();
     
-    CRTTrack(const geo::Point_t &_start, const geo::Point_t &_dir, const double &_time, const double &_etime,
-             const double &_pe, const double &_tof, const bool &_triple,
-             const std::array<bool, 7> &_taggers);
+    CRTTrack(const geo::Point_t &_start, const geo::Point_t &_end, const double &_time, const double &_etime,
+             const double &_pe, const double &_tof, const std::set<CRTTagger> &_taggers);
 
-    CRTTrack(const geo::Point_t &_start, const geo::Point_t &_dir, const double &_time, const double &_etime,
-             const double &_pe, const double &_tof, const bool &_triple,
-             const std::set<CRTTagger> &_used_taggers);
+    CRTTrack(const std::vector<geo::Point_t> &_points, const double &_time, const double &_etime,
+             const double &_pe, const double &_tof, const std::set<CRTTagger> &_taggers);
 
     virtual ~CRTTrack();
 
-    geo::Point_t        Start() const;
-    geo::Point_t        End() const;
-    double              Time() const;
-    double              TimeErr() const;
-    double              PE() const;
-    double              ToF() const;
-    bool                Triple() const;
-    std::array<bool, 7> Taggers() const;
+    std::vector<geo::Point_t> Points() const;
+    double                    Time() const;
+    double                    TimeErr() const;
+    double                    PE() const;
+    double                    ToF() const;
+    std::set<CRTTagger>       Taggers() const;
 
+    geo::Point_t  Start() const;
+    geo::Point_t  End() const;
     geo::Vector_t Direction() const;
     double        Length() const;
     double        Theta() const;
     double        Phi() const;
+    bool          Triple() const;
     
     bool UsedTagger(const CRTTagger tagger) const;
   };
