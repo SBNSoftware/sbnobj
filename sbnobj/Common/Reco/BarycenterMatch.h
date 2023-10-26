976a64efe4b610df0e4c4e25892695c64009581a
Jack Smedley <jsmedley@fnal.gov>
2023-10-26 18:53:35 -0500

    Fix sbnobj naming convention

--- sbnobj/Common/Reco/BarycenterMatch.h
+++ sbnobj/Common/Reco/BarycenterMatch.h
@@ -22,28 +22,28 @@ namespace sbn {
   static constexpr float fDefault = std::numeric_limits<float>::signaling_NaN();
 
   /// Data members related to the slice barycetner determination
-  float        fChargeTotal        { fDefault };                     ///< Total charge in slice contributing to barycenter
-  float        fChargeCenterXLocal { fDefault };                     ///< Weighted mean X position of spacepoints, measured with respect to the cathode (cm)
-  geo::Point_t fChargeCenter       { fDefault, fDefault, fDefault }; ///< Weighted mean spacepoint position in X,Y,Z (cm)
-  geo::Point_t fChargeWidth        { fDefault, fDefault, fDefault }; ///< Weighted standard devitation of spacepoint position in X,Y,Z (cm)
+  float        chargeTotal        { fDefault };                     ///< Total charge in slice contributing to barycenter
+  float        chargeCenterXLocal { fDefault };                     ///< Weighted mean X position of spacepoints, measured with respect to the cathode (cm)
+  geo::Point_t chargeCenter       { fDefault, fDefault, fDefault }; ///< Weighted mean spacepoint position in X,Y,Z (cm)
+  geo::Point_t chargeWidth        { fDefault, fDefault, fDefault }; ///< Weighted standard devitation of spacepoint position in X,Y,Z (cm)
 
   /// Data members related to matched recob::OpFlash, also reachable by association 
-  float        fFlashTime          { fDefault };                     ///< Matched OpFlash time (us)
-  float        fFlashFirstHit      { fDefault };                     ///< Time of first OpHit in matched OpFlash (us)
-  float        fFlashPEs           { fDefault };                     ///< Total PEs in matched flash
-  float        fFlashAsymmetry     { fDefault };                     ///< East-West asymmetry of PEs in matched flash
-  geo::Point_t fFlashCenter        { fDefault, fDefault, fDefault }; ///< Weighted mean ophit position in X,Y,Z [no meaingful X info for ophits] (cm)
-  geo::Point_t fFlashWidth         { fDefault, fDefault, fDefault }; ///< Weighted standard devitation of ophit position in X,Y,Z [no meaingful X info for ophits] (cm)
+  float        flashTime          { fDefault };                     ///< Matched OpFlash time (us)
+  float        flashFirstHit      { fDefault };                     ///< Time of first OpHit in matched OpFlash (us)
+  float        flashPEs           { fDefault };                     ///< Total PEs in matched flash
+  float        flashAsymmetry     { fDefault };                     ///< East-West asymmetry of PEs in matched flash
+  geo::Point_t flashCenter        { fDefault, fDefault, fDefault }; ///< Weighted mean ophit position in X,Y,Z [no meaingful X info for ophits] (cm)
+  geo::Point_t flashWidth         { fDefault, fDefault, fDefault }; ///< Weighted standard devitation of ophit position in X,Y,Z [no meaingful X info for ophits] (cm)
 
 
   /// Data members related to quality of match
-  float        fDeltaY             { fDefault };                     ///< abs( Matched flash Y center - charge Y center ) (cm)
-  float        fDeltaZ             { fDefault };                     ///< abs( Matched flash Z cetner - charge Z center ) (cm)
-  float        fRadius             { fDefault };                     ///< Hypotenuse of DeltaY and DeltaZ, PARAMETER MINIMIZED BY MATCHING (cm)
-  float        fDeltaT             { fDefault };                     ///< abs( Matched flash time - anab::T0 ) when available (us)
-  float        fOverlapY           { fDefault };                     ///< Spacial overlap of flash and charge centroids in Y [>0] OR distance apart if no overlap [<0] (cm)
-  float        fOverlapZ           { fDefault };                     ///< Spacial overlap of flash and charge centroids in Z [>0] OR distance apart if no overlap [<0] (cm)
-  float        fDeltaZ_Trigger     { fDefault };                     ///< abs( Triggering flash Z cetner - charge Z center ) (cm)
+  float        deltaY             { fDefault };                     ///< abs( Matched flash Y center - charge Y center ) (cm)
+  float        deltaZ             { fDefault };                     ///< abs( Matched flash Z cetner - charge Z center ) (cm)
+  float        radius             { fDefault };                     ///< Hypotenuse of DeltaY and DeltaZ, PARAMETER MINIMIZED BY MATCHING (cm)
+  float        deltaT             { fDefault };                     ///< abs( Matched flash time - anab::T0 ) when available (us)
+  float        overlapY           { fDefault };                     ///< Spacial overlap of flash and charge centroids in Y [>0] OR distance apart if no overlap [<0] (cm)
+  float        overlapZ           { fDefault };                     ///< Spacial overlap of flash and charge centroids in Z [>0] OR distance apart if no overlap [<0] (cm)
+  float        deltaZ_Trigger     { fDefault };                     ///< abs( Triggering flash Z cetner - charge Z center ) (cm)
 
   };
 }
