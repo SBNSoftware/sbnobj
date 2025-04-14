7b6c5e6223efe960615bcfa62b498c53049eaf8e
Alexander Antonakis <aantonakis@ucsb.edu>
2025-04-14 01:22:26 -0500

    Added descriptions for Veto Variants. Reset the Class Version to 10

--- sbnobj/SBND/CRT/CRTVeto.hh
+++ sbnobj/SBND/CRT/CRTVeto.hh
@@ -10,26 +10,42 @@
 #ifndef SBND_CRTVETO_HH
 #define SBND_CRTVETO_HH
 
-#include <set>
-#include <vector>
-#include "larcoreobj/SimpleTypesAndConstants/geo_vectors.h"
-
 namespace sbnd::crt {
 
   class CRTVeto {
+    
+   /**
+    * Brief Description of Veto Variables:
+    *
+    * V0 --> veto on any CRT activity minus the CRT bottom tagger
+    *
+    * V1 --> Same as V0 but require that both Top Taggers were hit to veto 
+    * on the CRT Top 
+    *
+    * V2 --> Same as V0 but do not veto on the North CRT Wall
+    *
+    * V3 --> Same as V2 but require that both Top Taggers were hit to veto 
+    * on the CRT Top
+    *
+    * V4 --> Only Veto on the South CRT Wall 
+    *
+    * CRT activity that is used to veto lies within a fcl configureable time
+    * window. This Window is chosen to be a narrow window around the 1.6 us beam 
+    * window in the nominal configuration
+    *
+    */
+ 
     bool fV0;    
     bool fV1;    
     bool fV2;    
     bool fV3;    
     bool fV4;    
-    std::vector<double> fSouthTimes;
 
   public:
 
     CRTVeto();
     
-    CRTVeto(bool _v0, bool _v1, bool _v2, bool _v3, bool _v4, const std::vector<double> _south_times);
-
+    CRTVeto(bool _v0, bool _v1, bool _v2, bool _v3, bool _v4);
 
     virtual ~CRTVeto();
 
@@ -38,9 +54,7 @@ namespace sbnd::crt {
     bool V2() const;
     bool V3() const;
     bool V4() const;
-    std::vector<double> SouthTimes() const;
 
-    //CRTVeto& operator= (CRTVeto const&) = default;
   };
 }
 
