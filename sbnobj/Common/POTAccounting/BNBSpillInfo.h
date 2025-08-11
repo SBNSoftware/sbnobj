b9c3b0a26ca3d617deaf2d45c1721c78cd21d388
Max <madubnowski@gmail.com>
2025-08-11 16:34:05 -0500

    test Change

--- sbnobj/Common/POTAccounting/BNBSpillInfo.h
+++ sbnobj/Common/POTAccounting/BNBSpillInfo.h
@@ -7,43 +7,31 @@
  * @author Joseph Zennamo, FNAL (jaz8600@fnal.gov)
  */
 
-#include <vector>
 
 namespace sbn {
+
   class BNBSpillInfo {
   public:
-    //typedef enum triggertype { kBNB, kEXT, kUnbiased, kNUMI } TriggerType; //!< Which beamline are we looking at
-    
-    unsigned long int spill_time_s; //!< The IFDB Beam Spill Time, unit sec
-    unsigned long int spill_time_ns; //!< The IFDB Beam Spill Time, unit nsec
-
     unsigned int event;
-        
-    float FOM; //Figure of Merit for BNB
 
+    float FOM; // Figure of Merit for BNB
 
     float TOR860; //!< Toroid before Mag 875, units e12 Protons
     float TOR875; //!< Toroid after Mag 875, units e12 Protons
-    
+
     float LM875A; //!< Loss Monitor before the RWM, unit R/s
     float LM875B; //!< Loss Monitor after the RWM, unit R/s
     float LM875C; //!< Loss Monitor after the RWM, unit R/s
+
     float HP873; //!< Horizontal Position Monitor after Mag 873, units mm
     float VP873; //!< Verticle Position Monitor after Mag 873, units mm
+
     float HP875; //!< Horizontal Position Monitor after Mag 875, units mm
     float VP875; //!< Verticle Position Monitor after Mag 875, units mm
 
-    float HPTG1; //!< Horizontal Position Monitor at Target Station 1, units mm 
-    float VPTG1; //!< Horizontal Position Monitor at Target Station 1, units mm
+    float THCURR; //!< Current applied to Horn, units kiloAmperes
 
-    float HPTG2; //!< Horizontal Position Monitor at Target Station 2, closest to target, units mm
-    float VPTG2; //!< Horizontal Position Monitor at Target Station 2, closest to target, units mm
-    
-    float BTJT2; //!< Temperature of air exiting target, units Deg C
-
-    float THCURR; //!< Current applied to Horn, units kiloAmperes 
-
-
+    // Keep the original multiwire stations from develop (since you removed them)
     float M875HS; //!< Multiwire station after Mag 875, Fit to Horizontal Sigma?
     float M875VS; //!< Multiwire station after Mag 875, Fit to Vertical Sigma?
     float M875HM; //!< Multiwire station after Mag 875, Fit to Horizontal Mean?
@@ -54,7 +42,7 @@ namespace sbn {
     float M876HM; //!< Multiwire station after Mag 876, Fit to Horizontal Mean?
     float M876VM; //!< Multiwire station after Mag 876, Fit to Vertical Mean?
 
-    float VP873; //units mm; 
+
     float HP875Offset;//units mm
     float VP875Offset;//units mm
     float VP873Offset;//units mm
@@ -74,11 +62,11 @@ namespace sbn {
     float M876BB_spill_time_diff; //!< the time difference between M876BB and the matched spill
     float MMBTBB_spill_time_diff; //!< the time difference between MMBTBB and the matched spill
 
-    double POT() const{return TOR875;}
-   
 
-    
+    double POT() const { return TOR875; }
+
   };
+
 } // end namespace sbn
 
 #endif
