892db00f2f3f50fc6890a575fbd0db23bbbfb935
VCLanNguyen <vclnguyen@ucsb.edu>
2025-09-02 11:18:54 -0500

    address doxygen comments and classes def

--- sbnobj/SBND/Timing/TimingInfo.hh
+++ sbnobj/SBND/Timing/TimingInfo.hh
@@ -1,6 +1,6 @@
 /*
  * @file   sbnobj/SBND/Timing/TimingInfo.hh
- * @brief  Defines data structures for SBND Timing Product (docdb#43090).
+ * @brief  Defines data structures for SBND Timing products (docdb#43090).
  * @author Vu Chi Lan Nguyen
  * @date   August 29, 2025
  *
@@ -10,79 +10,79 @@
 #define SBND_TIMINGINFO_HH
 
 #include <stdint.h>
-#include <string>
-#include <array>
-#include <limits> 
 
 namespace sbnd::timing {
 
-  /*
-   * @brief A struct to store important timestamps in SBND Data
+  /**
+   * @brief A class to store important timestamps in SBND Data
    * 
    * Each timestamp is in UNIX Timestamp Format [ns]
    *             
+   * For more information, see
+   * [SBN DocDB 43090](https://sbn-docdb.fnal.gov/cgi-bin/sso/ShowDocument?docid=43090).
    */
 
   class TimingInfo {
+  
+  public:
+    static constexpr uint64_t InvalidTimestamp = 0; ///< Invalid timestamp
 
-    uint64_t fRawDAQHeaderTimestamp; ///< Timestamp when the event is built by the event builder at DAQ-level
-    uint64_t fTdcCrtt1; ///< Timestamp of BNB stream CRT T1 Reset recorded by the SPEC-TDC
-    uint64_t fTdcBes; ///< Timestamp of BES signal sent by MFTU recorded by the SPEC-TDC
-    uint64_t fTdcRwm; ///< Timestamp of RWM signal recorded by the SPEC-TDC
-    uint64_t fTdcEtrig; ///< Timestamp of Event Trigger (ETRIG) sent by the PTB recorded by the SPEC-TDC 
-    uint64_t fHltCrtt1; ///< Timestamp of BNB and Offbeam stream CRT T1 Reset High Level Trigger (HLT) created by the PTB
-    uint64_t fHltEtrig; ///< Timestamp of ETRIG HLT created by the PTB
-    uint64_t fHltBeamGate; ///< Timestamp of Beam Gate Acceptance HLT created by the PTB
+  private:
+
+    uint64_t fRawDAQHeaderTimestamp = InvalidTimestamp; ///< Timestamp when the event is built by the event builder at DAQ-level
+    uint64_t fTdcCrtt1 = InvalidTimestamp; ///< Timestamp of BNB stream CRT T1 Reset recorded by the SPEC-TDC
+    uint64_t fTdcBes = InvalidTimestamp; ///< Timestamp of BES signal sent by MFTU recorded by the SPEC-TDC
+    uint64_t fTdcRwm = InvalidTimestamp; ///< Timestamp of RWM signal recorded by the SPEC-TDC
+    uint64_t fTdcEtrig = InvalidTimestamp; ///< Timestamp of Event Trigger (ETRIG) sent by the PTB recorded by the SPEC-TDC 
+    uint64_t fHltCrtt1 = InvalidTimestamp; ///< Timestamp of BNB and Offbeam stream CRT T1 Reset High Level Trigger (HLT) created by the PTB
+    uint64_t fHltEtrig = InvalidTimestamp; ///< Timestamp of ETRIG HLT created by the PTB
+    uint64_t fHltBeamGate = InvalidTimestamp; ///< Timestamp of Beam Gate Acceptance HLT created by the PTB
 
    public:
 
-    /*
+    /**
      * Default constructor.
      */
-    TimingInfo();
+    TimingInfo() = default;
 
     /*
      * Constructor to set all timestamps
      *
-     * @param _rawDAQHeaderTimestamp Raw DAQ Timestamp in UNIX format [ns]
-     * @param _tdcCrtt1 CRT T1 Timestamp in UNIX format [ns]
-     * @param _tdcBes BES Timestamp recorded by TDC in UNIX format [ns]
-     * @param _tdcRwm RWM Timestamp recorded by TDC in UNIX format [ns]
-     * @param _tdcEtrig ETRIG Timestamp recorded by TDC in UNIX format [ns]
-     * @param _hltCrtt1 CRT T1 Timestamp created by PTB in UNIX format [ns]
-     * @param _hltEtrig ETRIG Timestamp created by PTB in UNIX format [ns]
-     * @param _hltBeamGate Beam Gate Timestamp created by PTB in UNIX format [ns]
+     * @param rawDAQHeaderTimestamp Raw DAQ Timestamp in UNIX format [ns]
+     * @param tdcCrtt1 CRT T1 Timestamp in UNIX format [ns]
+     * @param tdcBes BES Timestamp recorded by TDC in UNIX format [ns]
+     * @param tdcRwm RWM Timestamp recorded by TDC in UNIX format [ns]
+     * @param tdcEtrig ETRIG Timestamp recorded by TDC in UNIX format [ns]
+     * @param hltCrtt1 CRT T1 Timestamp created by PTB in UNIX format [ns]
+     * @param hltEtrig ETRIG Timestamp created by PTB in UNIX format [ns]
+     * @param hltBeamGate Beam Gate Timestamp created by PTB in UNIX format [ns]
      */
-    TimingInfo(uint64_t _rawDAQHeaderTimestamp, uint64_t _tdcCrtt1, uint64_t _tdcBes, uint64_t _tdcRwm, uint64_t _tdcEtrig, uint64_t _hltCrtt1, uint64_t _hltEtrig, uint64_t _hltBeamGate);
+    TimingInfo(uint64_t rawDAQHeaderTimestamp, uint64_t tdcCrtt1, uint64_t tdcBes, uint64_t tdcRwm, uint64_t tdcEtrig, uint64_t hltCrtt1, uint64_t hltEtrig, uint64_t hltBeamGate);
 
-    /**
-     * Destructor
-     */
-    virtual ~TimingInfo();
-
-    /**
-     * Getters
-     */
-    uint64_t RawDAQHeaderTimestamp() const;
-    uint64_t TdcCrtt1() const;
-    uint64_t TdcBes() const;
-    uint64_t TdcRwm() const;
-    uint64_t TdcEtrig() const;
-    uint64_t HltCrtt1() const;
-    uint64_t HltEtrig() const;
-    uint64_t HltBeamGate() const;
+    /// @name Getters
+    /// @{
+    uint64_t RawDAQHeaderTimestamp() const { return fRawDAQHeaderTimestamp; }
+    uint64_t TdcCrtt1() const { return fTdcCrtt1; }
+    uint64_t TdcBes() const { return fTdcBes; }
+    uint64_t TdcRwm() const { return fTdcRwm; }
+    uint64_t TdcEtrig() const { return fTdcEtrig; }
+    uint64_t HltCrtt1() const { return fHltCrtt1; }
+    uint64_t HltEtrig() const { return fHltEtrig; }
+    uint64_t HltBeamGate() const { return fHltBeamGate; }
+    /// @}
  
-    /**
-     * Setters
-     */
-    void SetRawDAQHeaderTimestamp(uint64_t timestamp);
-    void SetTdcCrtt1(uint64_t timestamp);
-    void SetTdcBes(uint64_t timestamp);
-    void SetTdcRwm(uint64_t timestamp);
-    void SetTdcEtrig(uint64_t timestamp);
-    void SetHltCrtt1(uint64_t timestamp);
-    void SetHltEtrig(uint64_t timestamp);
-    void SetHltBeamGate(uint64_t timestamp);
+    /// @name Setters
+    /// @{
+    void SetRawDAQHeaderTimestamp(uint64_t timestamp){ fRawDAQHeaderTimestamp = timestamp; }
+    void SetTdcCrtt1(uint64_t timestamp){ fTdcCrtt1 = timestamp; }
+    void SetTdcBes(uint64_t timestamp){ fTdcBes = timestamp; }
+    void SetTdcRwm(uint64_t timestamp){ fTdcRwm = timestamp; }
+    void SetTdcEtrig(uint64_t timestamp){ fTdcEtrig = timestamp; }
+    void SetHltCrtt1(uint64_t timestamp){ fHltCrtt1 = timestamp; }
+    void SetHltEtrig(uint64_t timestamp){ fHltEtrig = timestamp; }
+    void SetHltBeamGate(uint64_t timestamp){ fHltBeamGate = timestamp; }
+    /// @}
+
   };
 }
 
