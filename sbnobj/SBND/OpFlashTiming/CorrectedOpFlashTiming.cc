5251119a734dc062e806968de36fdfae18f2d320
Alejandro Sánchez Castillo <asanchezcastillo@ugr.es>
2025-09-08 11:09:06 -0500

    Remove ol location

--- sbnobj/SBND/OpFlashTiming/CorrectedOpFlashTiming.cc
+++
@@ -1,1 +0,0 @@
-#include "sbnobj/SBND/OpFlashTiming/CorrectedOpFlashTiming.h"
--- sbnobj/SBND/OpFlashTiming/classes.h
+++
@@ -1,5 +0,0 @@
-#include "canvas/Persistency/Common/Wrapper.h"
-#include "canvas/Persistency/Common/Assns.h"
-#include "sbnobj/SBND/OpFlashTiming/CorrectedOpFlashTiming.h"
-#include "lardataobj/RecoBase/Slice.h"
-#include "lardataobj/RecoBase/OpFlash.h"
