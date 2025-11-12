d0b2cef51bbbbe7b315884c4920a1f386e78e0e2
VCLanNguyen <vclnguyen@ucsb.edu>
2025-11-11 18:24:00 -0600

    add timig channel

--- sbnobj/SBND/Timing/FrameShiftInfo.cxx
+++ sbnobj/SBND/Timing/FrameShiftInfo.cxx
@@ -12,14 +12,14 @@
 #include "sbnobj/SBND/Timing/FrameShiftInfo.hh"
 
 namespace sbnd::timing {
-  FrameShiftInfo::FrameShiftInfo(uint64_t frameCrtt1, uint16_t timingTypeCrtt1,
-                                 uint64_t frameBeamGate, uint16_t timingTypeBeamGate,
-                                 uint64_t frameEtrig, uint16_t timingTypeEtrig,
-                                 uint64_t frameDefault,  uint16_t timingTypeDefault)
-    : fFrameCrtt1(frameCrtt1), fTimingTypeCrtt1(timingTypeCrtt1),
-      fFrameBeamGate(frameBeamGate), fTimingTypeBeamGate(timingTypeBeamGate),
-      fFrameEtrig(frameEtrig), fTimingTypeEtrig(timingTypeEtrig),
-      fFrameDefault(frameDefault), fTimingTypeDefault(timingTypeDefault)
+  FrameShiftInfo::FrameShiftInfo(uint64_t frameCrtt1, uint16_t timingTypeCrtt1, uint16_t timingChannelCrtt1,
+                                 uint64_t frameBeamGate, uint16_t timingTypeBeamGate, uint16_t timingChannelBeamGate,
+                                 uint64_t frameEtrig, uint16_t timingTypeEtrig, uint16_t timingChannelEtrig,
+                                 uint64_t frameDefault,  uint16_t timingTypeDefault, uint16_t timingChannelDefault)
+    : fFrameCrtt1(frameCrtt1), fTimingTypeCrtt1(timingTypeCrtt1), fTimingChannelCrtt1(timingChannelCrtt1),
+      fFrameBeamGate(frameBeamGate), fTimingTypeBeamGate(timingTypeBeamGate), fTimingChannelBeamGate(timingChannelBeamGate),
+      fFrameEtrig(frameEtrig), fTimingTypeEtrig(timingTypeEtrig), fTimingChannelEtrig(timingChannelEtrig),
+      fFrameDefault(frameDefault), fTimingTypeDefault(timingTypeDefault), fTimingChannelDefault(timingChannelDefault)
   {}
 }
 
