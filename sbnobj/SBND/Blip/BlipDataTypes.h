55f7c5da17fecce0c6fd18e11bbac556fbab7854
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2026-01-05 16:37:42 -0600

    removing the thing I added

--- sbnobj/SBND/Blip/BlipDataTypes.h
+++ sbnobj/SBND/Blip/BlipDataTypes.h
@@ -2,7 +2,6 @@
 #define BLIPDATATYPE
 #include "lardataobj/RecoBase/Hit.h"
 #include "nusimdata/SimulationBase/MCParticle.h"
-#include "larcoreobj/SimpleTypesAndConstants/geo_vectors.h"
 #include "larcore/Geometry/Geometry.h"
 #include <vector>
 #include <map>
