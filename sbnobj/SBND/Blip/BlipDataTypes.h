b63c719cab5329dbff5a0e89fce97a6a60433066
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2026-02-10 11:31:52 -0600

    addressing doxygen comments

--- sbnobj/SBND/Blip/BlipDataTypes.h
+++ sbnobj/SBND/Blip/BlipDataTypes.h
@@ -11,34 +11,38 @@ namespace blip {
   const int kNplanes    = 3; ///< Wire planes in a given tpc in this detector
   const int kNTPCs      = 2; ///< TPC in this detector
 
-  /** Extra struct used in blip reco for grabbing key recob::hit information
- * Most attributes are grabbed directly from the recob::hit, but some, like charge, involve extra processing.
+  /** Extra struct used in blip reco for grabbing key recob::Hit information
+ * Most attributes are grabbed directly from the recob::Hit, but some, like charge, involve extra processing.
  */
   struct HitInfo {
-    int   hitid         = -9; ///< Index of hit in recob::hit vector
+    int   hitid         = -9; ///< Index of hit in recob::Hit vector
     int   cryo          = -9; ///< cryostat containing hit. From geo::WireID
     int   tpc           = -9; ///< TPC containing hit. From geo::WireID
     int   plane         = -9; ///< Plane containing hit. From geo::WireID
     int   wire          = -9; ///< Wire containing hit. From geo::WireID
-    int   chan          = -9; ///< Channel ID containing hit. From recob::Hit->Channel()
+    int   chan          = -9; ///< Channel ID containing hit. From recob::Hit::Channel()
     float amp           = -9; ///< Amplitude of hit [ADC]
     float rms           = -9; ///< RMS of hit shape [ticks]
     int   trkid         = -9; ///< track ID from hit-track association object
-    int   shwrid        = -9; ///< Depreciated
+    int   shwrid        = -9; ///< Deprecated
     int   clustid       = -9; ///< If this hit gets gathered into a cluster, what is the index of that cluster. All hits should be in a track or a cluster
     int   blipid        = -9; ///< If this hit gets gathered into a blip, what is the index of that blip.
-    bool  ismatch       = false; ///< Depreciated
-    float integralADC    = -999;  ///< Integral area from the recob::hit [ADCs-ticks]
+    bool  ismatch       = false; ///< Deprecated
+    float integralADC    = -999;  ///< Integral area from the recob::Hit [ADCs-ticks]
     float sigmaintegral = -999;   ///< Uncertainty on the integralADC value [ADC-ticks]
     float sumADC        = -999;   ///<  [ADCs] from ROISummedADC method
     float charge        = -999;    ///<  Total charge as estimated from hit integral [e-]
-    /*!
-      Charge is reconstructed from calo::CalorimetryAlg ( "sbnd_calorimetryalgmc" )-> ElectronsFromADCArea function
-      Configuration is in sbndcode/sbndcode/LArSoftConfigurations/calorimetry_sbnd.fcl
-    */
+     /**
+     * @brief Total charge as estimated from hit [e-]
+     * 
+     * Charge is reconstructed from calo::CalorimetryAlg::ElectronsFromADCArea().
+     * .
+     * SBND configuration is in `sbnd_calorimetryalgmc` in
+     * `sbndcode/LArSoftConfigurations/calorimetry_sbnd.fcl`
+     */
     float peakTime      = -999999; ///< Hit peak time + a fcl-set per-plane offset [tick]
     float driftTime     = -999999; ///< Drift-time accounting for the interplane separation corrections [tick]
-    float gof           = -9;     ///< GoodnessOfFit from recob::hit
+    float gof           = -9;     ///< from recob::Hit::GoodnessOfFit()
     int   g4trkid       = -9;    ///< simb::MCParticle g4 track ID associated with this hit
     int   g4pdg         = -999;  ///< simb::MCParticle g4pdg
     int   g4charge      = -999;   ///< anab::BackTrackerHitMatchingData numelectrons [e-]
@@ -59,7 +63,7 @@ namespace blip {
     int       Cryostat      = -9;     ///<  Cryostat ID the blip reconstructed to
     int       TPC           = -9;     ///<  TPC ID the blip reconstructed to
     float     Time          = -9;     ///<  Charge weighted peak time of TrueBlip energy depositions [tick]
-    int       TimeTick      = -9;     ///<  Depreciated
+    int       TimeTick      = -9;     ///<  Deprecated
     float     DriftTime     = -9;     ///<  Charge weighted drift time of TrueBlip energy depositions [tick]
     float     Energy        = 0;      ///<  Total energy dep [MeV]
     int       DepElectrons  = 0;      ///<  Total deposited electrons [e-]
@@ -68,7 +72,7 @@ namespace blip {
     int       LeadG4Index   = -9;     ///<  G4 track Index depositing the most charge in this deposition 
     int       LeadG4PDG     = -9;     ///<  G4 PDG associated with the track depositing the most charge
     float     LeadCharge    = -9;     ///<  Largest charge deposition associated with this True Blip
-    geo::Point_t  Position;               ///<  Charge weighted true-XYZ position [cm] //Used to be TVector3
+    geo::Point_t  Position;               ///<  Charge weighted true-XYZ position [cm] 
     std::map<int,float>      G4ChargeMap; ///< Map from G4 particle track ID to deposited charge in this energy deposition  
     std::map<int,float>      G4PDGMap; ///< Map from G4 particle track ID to G4 PDG
   };
@@ -81,11 +85,11 @@ namespace blip {
     simb::MCParticle particle;
     int   trackId           = -9;   ///< ID from GEANT4 track.
     int   index             = -9;   ///< Index in vector of simb::MCParticle 
-    int   isPrimary         = -9;   ///< Bool value of simbMCParticle is Primary
-    int   numTrajPts          = -9; ///< part.NumberTrajectoryPoints() from mcparticle
+    int   isPrimary         = -9;   ///< Bool value of simb::MCParticle is Primary
+    int   numTrajPts          = -9; ///< From simb::MCParticle::NumberTrajectoryPoints()
     double depEnergy         = -9;  ///< Total energy deposited across all interactions from this partcle [MeV]
     int   depElectrons        = -9; ///< Total electrons deposited across all interactions from this partcle [e-]
-    double numElectrons      = -9;  ///< Depreciated
+    double numElectrons      = -9;  ///< Deprecated
     double mass              = -9;  ///< simb::MCParticle mass [MeV]
     double E                 = -9;  ///< Starting simb::MCParticle energy [MeV]
     double endE              = -9;  ///< simb::MCParticle final energy [MeV]
@@ -95,12 +99,12 @@ namespace blip {
     double Px                = -9;  ///< X-component of the momentum vector [MeV/c]
     double Py                = -9;  ///< Y-component of the momentum vector [MeV/c]
     double Pz                = -9;  ///< Z-component of the momentum vector [MeV/c]
-    double pathLength        = -9; ///< Sum of distances between interaction vertecies within the active volume [cm]
+    double pathLength        = -9; ///< Sum of distances between interaction vertices within the active volume [cm]
     double time              = -9; ///< Time at birth [us]
     double endtime           = -9; ///< Time of final interaction [us]
-    geo::Point_t startPoint;           ///< Starting Position of particle [cm] //Used to be TVector3
-    geo::Point_t endPoint;             ///< End position of particle [cm]  //Used to be TVector3
-    geo::Point_t Position;             ///< Central position of the trajectory [cm]  //Used to be TVector3
+    geo::Point_t startPoint;           ///< Starting Position of particle [cm] 
+    geo::Point_t endPoint;             ///< End position of particle [cm]  
+    geo::Point_t Position;             ///< Central position of the trajectory [cm]  
   };
 
  /** Hit clusters are collections of adjacent hits on a single wire plane.
@@ -115,12 +119,14 @@ namespace blip {
     int     CenterChan      = -999;  ///< Channel ID of the wire in the geometric center of the hit cluster
     int     CenterWire      = -999;  ///< Wire ID of the wire in the geometric center of the hit cluster
     bool    isTruthMatched  = false; ///< is there a trueBlip with the same leadG4ID as one of the G4IDs making up this cluster
-    bool    isMerged        = false; ///< Depreciated internal flag for tracking blips before/after merge. 
+    bool    isMerged        = false; ///< Deprecated internal flag for tracking blips before/after merge. 
     bool    isMatched       = false; ///< Is this hit cluster plane-matched into a full 3d blip
     int     DeadWireSep     = 99;    ///< Separation between the extreme ends of the hitcluster and the nearest dead wire. 
-    /*!
-      DeadWireSep can be between 0 and 5 and valid. Larger separations are filled in as 99.
-    */
+    /**
+     * @brief Separation between the extreme ends of the hitcluster and the nearest dead wire.
+     * 
+     * `DeadWireSep` can be between `0` and `5` and valid. Larger separations are filled in as `99`.
+     */
     int     Cryostat        = -9;   ///< cryostat for this hit cluster
     int     TPC             = -9;   ///< TPC for this hit cluster
     int     Plane           = -9;   ///< Plane index for this hit cluster
@@ -129,15 +135,18 @@ namespace blip {
     float   ADCs            = -999; ///< ADC integral sum of hits making up the cluster [ADC-tick]
     float   Amplitude       = -999; ///< Max amplitude of hits making up the hit cluster [ADC]
     float   Charge          = -999; ///< Total charge of hits making up the hit cluster [e-]
-    /*!
-      Charge is reconstructed from calo::CalorimetryAlg ( "sbnd_calorimetryalgmc" )-> ElectronsFromADCArea function
-      Configuration is in sbndcode/sbndcode/LArSoftConfigurations/calorimetry_sbnd.fcl
+    /**
+     * @brief Total charge of hits making up the hit cluster [e-]
+     * 
+     * Charge is reconstructed from calo::CalorimetryAlg::ElectronsFromADCArea().
+     * In SBND, configuration is in `sbnd_calorimetryalgmc`
+     * from `sbndcode/LArSoftConfigurations/calorimetry_sbnd.fcl`.
     */
     float   SigmaCharge     = -999; ///< charge-weighted charge uncertainties for this hit-cluster [e-]
     float   TimeTick        = -999; ///< charge-weighted average hit-peak-times for this hit-cluster [tick]
     float   Time            = -999; ///< charge-weighted average hit-peak-times for this hit-cluster [us]
-    float   StartHitTime    = -999; ///< Depreciated
-    float   EndHitTime      = -999; ///< Depreciated
+    float   StartHitTime    = -999; ///< Deprecated
+    float   EndHitTime      = -999; ///< Deprecated
     float   StartTime       = -999; ///< Minimum -1 sigma time of a hit in this cluster [us]
     float   EndTime         = -999; ///< Max +1 sigma time of a hit in this cluster [us]
     float   Timespan        = -999; ///< Hit cluster EndTime - StartTime [us]
@@ -148,11 +157,11 @@ namespace blip {
     float   GoodnessOfFit   = -999; ///< Charge weighted hit-GoodnessofFit param
     int     BlipID          = -9; ///< If this hit cluster ended up in a blip, what is its ID
     int     EdepID          = -9; ///< If this hit cluster is MC-matched what is the trueBlip ID
-    std::set<int>     HitIDs; ///< Index of the recob::hit objects making up this cluster
-    std::set<int>     Wires; ///<  Set of geo::wireIDs contributing hits to this cluster
+    std::set<int>     HitIDs; ///< Index of the recob::Hit objects making up this cluster
+    std::set<int>     Wires; ///<  Set of geo::wireID contributing hits to this cluster
     std::set<int>     Chans; ///<  Set of raw::ChannelID_t contributing hits to this cluster
     std::set<int>     G4IDs; ///<  simb::MCParticle track ID contributing hits to this cluster
-    std::map<int,geo::Point_t> IntersectLocations; ///< Internal reconstruction variable for recording where hit-clusters on different planes would overlap //used to be TVector3
+    std::map<int,geo::Point_t> IntersectLocations; ///< Internal reconstruction variable for recording where hit-clusters on different planes would overlap 
   };
 
 
@@ -171,9 +180,17 @@ namespace blip {
     int       NPlanes         = -9;         ///< Number of planes matched to build the blip. Blips must be plane matched so this should be 2+ planes.
     int       MaxWireSpan     = -9;         ///< Maximum span of wires on any plane hit-cluster
     float     TimeTick        = -999;       ///< Average time of hitclusters making up blips. [tpc tick]
-    /*!
-      Hit cluster time is the charge-weighted average of the hit-peak times for hits in the cluster 
-    */
+    /**
+     * @brief Average time of hitclusters making up blips. [tpc tick]
+     * 
+     * Hit cluster time is the charge-weighted average of the hit-peak times for hits in the cluster.
+     */
+    float     TimeTick        = -999;
+    /**
+     * @brief Average time of hitclusters making up blips. [us]
+     * 
+     * Hit cluster time is the charge-weighted average of the hit-peak times for hits in the cluster.
+     */
     float     Time            = -999;       ///< Average time of hitclusters making up blips. [us] 
     /*!
       Hit cluster time is the charge-weighted average of the hit-peak times for hits in the cluster 
@@ -183,15 +200,21 @@ namespace blip {
     float     EnergyESTAR     = -999;       ///< Reconstructed energy in the calorimetry plane (ESTAR method from ArgoNeuT)    [MeV]
     float     EnergyPSTAR     = -999;       ///< Reconstructed energy in the calorimetry plane (PSTAR method similar with ESTAR method from ArgoNeuT) [MeV]
     float     ProxTrkDist     = -9;         ///< 3-D distance to closest track, assuming the blip was concident with event trigger [cm]
-    /*!
-      for properly flash matched out-of-time tracks this distance will be wrong! The blips have no such flash matching ability as of yet
-    */
+    /**
+     * @brief 3-D distance to closest track, assuming the blip was coincident with event trigger [cm]
+     * 
+     * For properly flash matched out-of-time tracks this distance will be wrong!
+     * The blips have no such flash matching ability as of yet.
+     */
     int       ProxTrkID       = -9;         ///< index of the of closest track, assuming the blip was concident with event trigger
     bool      inCylinder      = false;      ///< Bool for whether this blip falls in a 45 degree cone relative to any track closer than fcl-set CylinderRadius (15 cm)
-    /*!
-      please note the blip X position is unreliable, so these distance and 3-d position derived variables may be incorrect
-    */
     geo::Point_t  Position;                 ///< 3D position vector. Reconstructed with wrong t0! [cm]
+    /**
+     * @brief 3D position vector. Reconstructed with wrong t0! [cm]
+     * 
+     * Please note the blip X position is unreliable,
+     * so these distance and 3-d position derived variables may be incorrect.
+     */
     float     SigmaYZ         = -9.;        ///< Uncertainty in YZ intersect [cm]
     float     dX              = -9;         ///< Equivalent length along drift direction [cm] 
     float     dYZ             = -9;         ///< Approximate length scale in YZ space [cm]
